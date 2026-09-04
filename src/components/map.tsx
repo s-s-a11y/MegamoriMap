9db77005cee790cb95dbb4be3a9a79e64445b781
Sshiigai <itkonwa2026040605@gmail.com>
2026-09-04 14:21:24 +0900

    sasasasasaasasasasas

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from "react";
 import * as maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
 
 export function MapComponent() {
   const mapContainer = useRef(null);
