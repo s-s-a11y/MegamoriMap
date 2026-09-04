4c6787a6910a5fc9b5dc0f6c4a2914fdfbc0889c
Sshiigai <itkonwa2026040605@gmail.com>
2026-09-04 17:28:59 +0900

    view currentPosition

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+// maplibre-glをインポート(V6対応版)
 import * as maplibregl from "maplibre-gl";
+// 地図表示の際のstylesheetを読み込み
 import "maplibre-gl/dist/maplibre-gl.css";
 // worker本体をViteに正しくバンドルさせて、そのURLを取得する
 // (プレーンな ?url だとworkerが依存している maplibre-gl-shared.mjs が
@@ -9,29 +11,53 @@ import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 // アプリ起動時に一度だけ、workerの場所をMapLibreに教える
 maplibregl.setWorkerUrl(workerUrl);
 
-export function MapComponent() {
-  const mapContainer = useRef<HTMLDivElement | null>(null);
+interface Position {
+  latitude: number | null;
+  longitude: number | null;
+}
 
+export function MapComponent() {
+  // マップ表示用のDOMを取得する。
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  // 現在地取得用State
+  const [position, setPosition] = useState<Position>({
+    latitude: null,
+    longitude: null,
+  });
+  // 環境変数から値を取得する
   const apiKey = import.meta.env.VITE_MAP_API_KEY;
   const mapName = import.meta.env.VITE_MAP_NAME;
   const region = import.meta.env.VITE_AWS_REGION;
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setPosition({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      });
+    });
+  }, []);
+
+  //   画面表示とともにAmazone Location ServiceのMapsAPIをたたき地図データを取得、描画する
+  useEffect(() => {
     if (!mapContainer.current) return;
-
+    if (position.latitude === null || position.longitude === null) return;
+    // 接続先URLの成型：環境変数から取得した値をもとにして作成
     const styleUrl = `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor?key=${apiKey}`;
-
+    // 作成する地図の設定項目を書き込んで実際にAPIからデータを取得する
     const map = new maplibregl.Map({
       container: mapContainer.current,
       style: styleUrl,
-      center: [139.767125, 35.681236],
+      //   地図の中心とする座標
+      center: [position.longitude, position.latitude],
+      //   地図の縮尺レベルを定める
       zoom: 12,
     });
-
+    // 拡大/縮小ボタンの追加
     map.addControl(new maplibregl.NavigationControl(), "top-right");
-
+    // コンポーネントが描画されなくなると同時にデータを消去する処理(メモリリーク対策)
     return () => map.remove();
-  }, []);
+  }, [position]);
 
   return <div ref={mapContainer} style={{ width: "100%", height: "500px" }} />;
 }
