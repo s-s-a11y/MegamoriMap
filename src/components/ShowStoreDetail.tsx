b6685538800c9733033d194ef00bf0824e0ec6ae
Sshiigai <itkonwa2026040605@gmail.com>
2026-09-15 10:55:54 +0900

    change storemenus

--- src/components/ShowStoreDetail.tsx
+++ src/components/ShowStoreDetail.tsx
@@ -208,8 +208,7 @@ export function StoreDetailPage({ placeId, onNavigate }: StoreDetailPageProps) {
     setMenuErrorMessage(null);
 
     const apiUrl =
-      "https://uay8s2uqz9.execute-api.ap-northeast-1.amazonaws.com/MegamoriMap/megamorimap/ShowStoreMenues";
-
+      "https://uay8s2uqz9.execute-api.ap-northeast-1.amazonaws.com/MegamoriMap/stores/menus";
     fetch(apiUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
